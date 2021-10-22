da644cfb3bd40408938548c663f29572478d3e16
Johannes Lindgren <johannes.lindgren@pricefx.com>
2021-10-22 18:37:38 +0200

    Improved groovydocs for pl header lib

--- pricefxSrc/CalculationLogic/PriceListHeaderLib/elements/Input.groovy
+++ pricefxSrc/CalculationLogic/PriceListHeaderLib/elements/Input.groovy
@@ -1,4 +1,10 @@
+/**
+ * In price list header logics, returns the specified input value.
+ * @param inputName the name of the input
+ * @return the value of the input
+ */
 def getValue(String inputName){
+    // Mock if necessary
     if (api.debugMode) {
         api.trace("""
             Note: The input is mocked by reading from the input binding variable, because the code
@@ -7,6 +13,7 @@ def getValue(String inputName){
         """)
         return input[inputName]
     }
+
     // For price list header logics, api.currentItem() is only available in syntax check mode
     String configurationJson = api.currentItem().configuration
     Map configuration = api.jsonDecode(configurationJson) as Map
@@ -16,6 +23,15 @@ def getValue(String inputName){
             ?.value
 }
 
+/**
+ * With this method, you can read the inputs with the [] operators. For example:
+ * <pre>
+ *     def priceListHeaderInputs = libs.PriceListHeaderLib.Input
+ *     def inputValue = priceListHeaderInputs[inputName]
+ * <pre>
+ * @param inputName
+ * @return
+ */
 def getAt(String inputName) {
     return getValue(inputName)
 }
