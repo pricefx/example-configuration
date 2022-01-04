d700886c6f39df4e4ec3389298ecf8bc571e2f94
Johannes Lindgren <johannes.lindgren@pricefx.com>
2022-01-04 18:49:19 +0100

    Removed commented code

--- pricefxSrc/CalculationLogic/Dashboard_Chart_Examples_Form/elements/UserInputs.groovy
+++ pricefxSrc/CalculationLogic/Dashboard_Chart_Examples_Form/elements/UserInputs.groovy
@@ -10,18 +10,6 @@ def gaugeInput = api.inputBuilderFactory().createUserEntry(GAUGE_VALUE)
         .setFormatType('PERCENT')
         .buildContextParameter()
 
-//def quantityInput = api.inputBuilderFactory()
-//        .createIntegerUserEntry(QUANTITY_INPUT)
-//        .setLabel('Quantity')
-//        .setValue(input[QUANTITY_INPUT] as Integer)
-//        .buildContextParameter()
-//
-//def invoicePriceInput = api.inputBuilderFactory()
-//        .createUserEntry(INVOICE_PRICE_INPUT)
-//        .setLabel('Invoice Price')
-//        .setValue(input[INVOICE_PRICE_INPUT] as BigDecimal)
-//        .buildContextParameter()
-
 def gaugeFieldSet = api.createConfiguratorEntry()
 gaugeFieldSet.message = 'Gauge'
 gaugeFieldSet.inputs = [
