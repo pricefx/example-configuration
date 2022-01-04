31e24a58742ff06ed598be280f4adedd92f5ceac
Johannes Lindgren <johannes.lindgren@pricefx.com>
2022-01-04 15:35:21 +0100

    Prettified method

--- pricefxSrc/CalculationLogic/PriceList_CostPlus_Form/elements/Country.groovy
+++ pricefxSrc/CalculationLogic/PriceList_CostPlus_Form/elements/Country.groovy
@@ -1,19 +1,22 @@
+import groovy.transform.Field
 import net.pricefx.server.dto.calculation.ConfiguratorEntry
+
+@Field final String INPUT_COUNTRY_CODE = 'countryCode'
 
 return createCountryInput()
 
 ConfiguratorEntry createCountryInput() {
-    def fieldset = api.createConfiguratorEntry()
+    // Libs
     def countries = libs.Library_Geography.Country
+
+    def fieldset = api.createConfiguratorEntry()
     def countryCodeToName = countries.findCountryCodeToName()
     def allCountryCodes = countryCodeToName.keySet().toList()
-
-    final String INPUT_COUNTRY_CODE = 'countryCode'
 
     fieldset.inputs = [
             api.inputBuilderFactory().createOptionEntry(INPUT_COUNTRY_CODE)
                     .setValue(input?.getAt(INPUT_COUNTRY_CODE))
-                    .setLabel('Country')             // Label of the entire input
+                    .setLabel('Country')             // Label of the whole input
                     .setRequired(true)
                     .setOptions(allCountryCodes)    // Values of the options
                     .setLabels(countryCodeToName) // Labels of the options
