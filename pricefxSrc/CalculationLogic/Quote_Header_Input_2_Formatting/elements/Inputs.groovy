4eaa8effff63b61a83da3b0195f47bacdd2c8930
Pavel Matyasko <pavel.matyasko@pricefx.com>
2022-03-29 10:15:17 +0200

    Added samples of Input Field column layout on header

--- pricefxSrc/CalculationLogic/Quote_Header_Input_2_Formatting/elements/Inputs.groovy
+++ pricefxSrc/CalculationLogic/Quote_Header_Input_2_Formatting/elements/Inputs.groovy
@@ -10,5 +10,4 @@ if (quoteProcessor.isPrePhase()) {
 
     }
 
-
 }
--- pricefxSrc/CalculationLogic/Quote_Header_Input_2_Formatting/elements/Outputs.groovy
+++ pricefxSrc/CalculationLogic/Quote_Header_Input_2_Formatting/elements/Outputs.groovy
@@ -1,8 +1,7 @@
-import net.pricefx.common.api.CalculationResultType
-
 if (quoteProcessor.isPostPhase()) {
 
     def displaySettings = [numberOfFieldsPerLine: 3]
+
     quoteProcessor.addOrUpdateOutput(
             [
                     "resultName": "__UI_HeaderDisplaySettings",
@@ -10,19 +9,5 @@ if (quoteProcessor.isPostPhase()) {
                     "result"    : displaySettings
             ]
     )
-
-
-    for (int i = 1; i <= 4; i++) {
-        quoteProcessor.addOrUpdateOutput(
-                [
-                        "resultName"    : "Output$i".toString(),
-                        "resultLabel"   : "Output$i".toString(),
-                        "resultType"    : CalculationResultType.SIMPLE,
-                        "result"        : i
-                ]
-        )
-
-    }
-
 
 }
