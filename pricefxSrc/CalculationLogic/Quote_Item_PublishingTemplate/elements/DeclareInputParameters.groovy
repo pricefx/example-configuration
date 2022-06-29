74d5e2eaa0b2b18dfd74e685ea0cede662dbd49c
Pavel Matyasko <pavel.matyasko@pricefx.com>
2022-06-29 12:45:19 +0200

    Adjusted range for input field.

--- pricefxSrc/CalculationLogic/Quote_Item_PublishingTemplate/elements/DeclareInputParameters.groovy
+++ pricefxSrc/CalculationLogic/Quote_Item_PublishingTemplate/elements/DeclareInputParameters.groovy
@@ -3,21 +3,21 @@ if (api.isSyntaxCheck()) {
 
 //     Quantity
     api.inputBuilderFactory().createIntegerUserEntry("Quantity")
-            .setInputType("range")
-            .setMin(1)      // Limit values to be > 0
             .setLabel("Required Quantity")
             .setRequired(true)
+            //.setInputType("range")  //not needed anymore
+            .setFrom(1)      // Limit values to be > 0
             .getInput()
 
 //     Sales Discount %
     api.inputBuilderFactory().createUserEntry("SalesDiscountPct")
-            .setValue(0) // Set initial value
-            .setInputType("range")
-            .setMin(0)  // Min value
-            .setMax(1)  // Max value
-            .setFormatType("PERCENT")
             .setLabel("Sales Discount (%)")
             .setRequired(false)
+            .setFormatType("PERCENT")
+            .setValue(0) // Set initial value
+            //.setInputType("range")  //not needed anymore
+            .setFrom(0)  // Min value
+            .setTo(1)  // Max value - regarding the real value kept in the input
             .getInput()
 
 }
